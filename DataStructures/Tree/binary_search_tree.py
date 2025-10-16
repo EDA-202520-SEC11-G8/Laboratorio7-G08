27eebc9bf563419d696215b981392ece390a3a3a
Dantinja <d.tinjaca@uniandes.edu.co>
2025-10-16 18:09:30 -0500

    Get y get node

--- DataStructures/Tree/binary_search_tree.py
+++ DataStructures/Tree/binary_search_tree.py
@@ -4,3 +4,22 @@ def new_map():
         Crea un nuevo arbol
     """
     return {"root":None}
+
+def get_node(root, key):
+    k = bn.get_key(root)
+    if k is None:
+        return None
+    if key < k:
+        return get_node(root["left"], key)
+    if key > k:   
+        return get_node(root["right"], key)
+    if k == key:
+        return root
+    
+
+def get (my_bst, key):
+    
+    if my_bst["root"] is not None:
+        return None
+    node = get_node(my_bst["root"], key)
+    return bn.get_value(node)
